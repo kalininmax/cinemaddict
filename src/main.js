862ddbb1f007ddf590ddc0d7558aa19532ced6c7
Maksim Kalinin <kalinin.m.a@bk.ru>
2021-04-13 23:17:38 +0300

    Создаёт массив фильмов и рендерит их

--- src/main.js
+++ src/main.js
@@ -8,9 +8,10 @@ import { createShowMoreButtonTemplate } from './view/show-more-btn';
 import { createStatisticTemplate } from './view/statistics';
 import { createFilmsListExtraTemplate } from './view/films-list-extra';
 import { createFilmDetailsTemplate } from './view/film-details';
+import { generateFilm } from './mock/film';
 
-const MAIN_FILM_COUNT = 5;
-const EXTRA_FILM_COUNT = 2;
+const FILMS_COUNTER = 20;
+const filmItems = [];
 
 
 const render = (container, template, position = 'beforeend') => {
@@ -32,20 +33,17 @@ const filmsElement = mainElement.querySelector('.films');
 const filmsListElement = filmsElement.querySelector('.films-list');
 const filmListContainerElement = filmsElement.querySelector('.films-list__container');
 
-for (let i = 0; i < MAIN_FILM_COUNT; i++) {
-  render(filmListContainerElement, createFilmCardTemplate());
-}
-
 render(filmsListElement, createShowMoreButtonTemplate());
 render(filmsElement, createFilmsListExtraTemplate('Top rated'));
 render(filmsElement, createFilmsListExtraTemplate('Most commented'));
 
-const filmListExtraContainerElement = filmsElement.querySelectorAll('.films-list--extra .films-list__container');
+render(mainElement, createStatisticTemplate());
 
-for (let i = 0; i < EXTRA_FILM_COUNT; i++) {
-  render(filmListExtraContainerElement[0], createFilmCardTemplate());
-  render(filmListExtraContainerElement[1], createFilmCardTemplate());
+for (let i = 0; i < FILMS_COUNTER; i++) {
+  filmItems.push(generateFilm(i));
 }
 
-render(filmsElement, createFilmDetailsTemplate());
-render(mainElement, createStatisticTemplate());
+filmItems.forEach((film) => {
+  render(filmListContainerElement, createFilmCardTemplate(film));
+});
+render(filmsElement, createFilmDetailsTemplate(filmItems[0]));

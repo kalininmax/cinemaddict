8fa78af63c200e84537a11365f14c9af8e04e1cd
Maksim Kalinin <kalinin.m.a@bk.ru>
2021-04-13 23:18:10 +0300

    Изменяет генерацию комментариев

--- src/mock/comment.js
+++ src/mock/comment.js
@@ -1,11 +1,11 @@
 import dayjs from 'dayjs';
-import { getRandomNumber, getRandomElement } from './utils';
+import { getRandomNumber, getRandomElement, shuffleArray } from './utils';
 
 
 const USER_NAMES = ['Mark Matthews', 'Paul Skinner', 'John Webster', 'Leonard Bailey', 'Terence Bridges', 'Donald Oliver', 'James Hardy'];
 const COMMENTS = ['Boring movie', 'One time okay movie', 'Superb and extremely deeply intimidating moviesTeaches us all about the life', 'Awesome movie', 'I never watched movie like this this is a epic one must watch', 'Well writted and directed! deserved all the nominations and winnings. A must watch for all', 'This a greatest movie ever I have seen'];
 const EMOTION = ['smile', 'sleeping', 'puke', 'angry'];
-const COMMENTS_COUNTER = 7;
+const COMMENTS_COUNTER = 30;
 
 const generateDate = () => {
   const maxDaysGap = 20;
@@ -24,16 +24,17 @@ const generateComment = (id) => {
 };
 
 const generateComments = () => {
-  const count = getRandomNumber(0, COMMENTS_COUNTER);
   const comments = [];
-  for (let i = 0; i < count; i++) {
+  for (let i = 0; i < COMMENTS_COUNTER; i++) {
     comments.push(generateComment(i));
   }
   return comments;
 };
 
+const comments = generateComments();
+
 const getCommentIds = () => {
-  return generateComments().map(({ id }) => id);
+  return shuffleArray(comments).slice(0, getRandomNumber(1, 7)).map(({ id }) => id);
 };
 
 export { getCommentIds };

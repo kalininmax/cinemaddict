63a8c88a3e1e9d60ce142b3bc554af04c18ff6a5
Maksim Kalinin <kalinin.m.a@bk.ru>
2021-04-15 01:04:54 +0300

    Пытается связать данные с шаблоном

--- src/view/statistics.js
+++ src/view/statistics.js
@@ -1,4 +1,33 @@
-const createStatisticTemplate = () => {
+const createStatisticFiltersTemplate = ({ statistic_time }, isChecked) => {
+  if (statistic_time === 'all-time') {
+    return `<input type="radio"
+    class="statistic__filters-input visually-hidden"
+    name="statistic-filter"
+    id="statistic-${statistic_time}"
+    value="${statistic_time}" ${isChecked ? 'checked' : ''}>
+  <label for="statistic-${statistic_time}" class="statistic__filters-label">All time</label>`;
+  }
+
+  return `<input type="radio"
+    class="statistic__filters-input visually-hidden"
+    name="statistic-filter"
+    id="statistic-${statistic_time}"
+    value="${statistic_time}" ${isChecked ? 'checked' : ''}>
+  <label for="statistic-${statistic_time}" class="statistic__filters-label">${statistic_time}</label>`;
+};
+
+const calculateStatistic = (films) => {
+  return {
+    watched_films_count: films.length,
+    total_duration: films
+      .map(({ film_info: { runtime } }) => runtime)
+      .reduce((a, b) => a + b),
+  };
+};
+
+const createStatisticTemplate = (statistic) => {
+  const statisticFiltersTemplate = statistic.map((statistic, index) => createStatisticFiltersTemplate(statistic, index === 0)).join('');
+  const statisticData = calculateStatistic(statistic[0].watched_films);
   return `<section class="statistic">
     <p class="statistic__rank">
       Your rank
@@ -8,27 +37,13 @@ const createStatisticTemplate = () => {
 
     <form action="https://echo.htmlacademy.ru/" method="get" class="statistic__filters">
       <p class="statistic__filters-description">Show stats:</p>
-
-      <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-all-time" value="all-time" checked>
-      <label for="statistic-all-time" class="statistic__filters-label">All time</label>
-
-      <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-today" value="today">
-      <label for="statistic-today" class="statistic__filters-label">Today</label>
-
-      <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-week" value="week">
-      <label for="statistic-week" class="statistic__filters-label">Week</label>
-
-      <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-month" value="month">
-      <label for="statistic-month" class="statistic__filters-label">Month</label>
-
-      <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-year" value="year">
-      <label for="statistic-year" class="statistic__filters-label">Year</label>
+      ${statisticFiltersTemplate}
     </form>
 
     <ul class="statistic__text-list">
       <li class="statistic__text-item">
         <h4 class="statistic__item-title">You watched</h4>
-        <p class="statistic__item-text">22 <span class="statistic__item-description">movies</span></p>
+        <p class="statistic__item-text">${statisticData.watched_films_count}<span class="statistic__item-description">movies</span></p>
       </li>
       <li class="statistic__text-item">
         <h4 class="statistic__item-title">Total duration</h4>

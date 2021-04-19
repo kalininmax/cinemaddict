7310fd4ee53251681df3e5a80f46b0df1058ae60
Maksim Kalinin <kalinin.m.a@bk.ru>
2021-04-20 00:10:46 +0300

    Изменяет имя переменной и добавляет в экспорт

--- src/mock/comment.js
+++ src/mock/comment.js
@@ -4,7 +4,7 @@ import { getRandomNumber, getRandomElement, shuffleArray } from './utils';
 
 const USER_NAMES = ['Mark Matthews', 'Paul Skinner', 'John Webster', 'Leonard Bailey', 'Terence Bridges', 'Donald Oliver', 'James Hardy'];
 const COMMENTS = ['Boring movie', 'One time okay movie', 'Superb and extremely deeply intimidating moviesTeaches us all about the life', 'Awesome movie', 'I never watched movie like this this is a epic one must watch', 'Well writted and directed! deserved all the nominations and winnings. A must watch for all', 'This a greatest movie ever I have seen'];
-const EMOTION = ['smile', 'sleeping', 'puke', 'angry'];
+const EMOTIONS = ['smile', 'sleeping', 'puke', 'angry'];
 const COMMENTS_COUNTER = 30;
 
 const generateDate = () => {
@@ -19,7 +19,7 @@ const generateComment = (id) => {
     author: getRandomElement(USER_NAMES),
     comment: getRandomElement(COMMENTS),
     date: generateDate(),
-    emotion: getRandomElement(EMOTION),
+    emotion: getRandomElement(EMOTIONS),
   };
 };
 
@@ -37,4 +37,4 @@ const getCommentIds = () => {
   return shuffleArray(allComments).slice(0, getRandomNumber(1, 5)).map(({ id }) => id);
 };
 
-export { getCommentIds, allComments };
+export { getCommentIds, allComments, EMOTIONS };

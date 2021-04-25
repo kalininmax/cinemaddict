6d767f1b478dbc5c00f88c003da610eb1fb41429
Maksim Kalinin <kalinin.m.a@bk.ru>
2021-04-25 21:49:48 +0300

    Добавляет функцию замены компонентов

--- src/utils/render.js
+++ src/utils/render.js
@@ -30,6 +30,24 @@ const render = (container, element, place) => {
   }
 };
 
+const replace = (newChild, oldChild) => {
+  if (oldChild instanceof Abstract) {
+    oldChild = oldChild.getElement();
+  }
+
+  if (newChild instanceof Abstract) {
+    newChild = newChild.getElement();
+  }
+
+  const parent = oldChild.parentElement;
+
+  if (parent === null || oldChild === null || newChild === null) {
+    throw new Error('Can\'t replace unexisting elements');
+  }
+
+  parent.replaceChild(newChild, oldChild);
+};
+
 const remove = (component) => {
   if (!(component instanceof Abstract)) {
     throw new Error('Can remove only components');
@@ -42,6 +60,7 @@ const remove = (component) => {
 export {
   createElement,
   render,
+  replace,
   remove,
   RenderPosition
 };

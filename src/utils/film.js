8839052830cd5faf1fabe7611cd11091bde76322
Maksim Kalinin <kalinin.m.a@bk.ru>
2021-05-01 18:25:48 +0300

    Добавляет функции сравнения для метода sort

--- src/utils/film.js
+++ src/utils/film.js
@@ -4,4 +4,62 @@ const humanizeDate = (date, template = 'D MMMM YYYY') => {
   return dayjs(date).format(template);
 };
 
-export { humanizeDate };
+const getWeightForNullDate = (dateA, dateB) => {
+  if (dateA === null && dateB === null) {
+    return 0;
+  }
+
+  if (dateA === null) {
+    return 1;
+  }
+
+  if (dateB === null) {
+    return -1;
+  }
+
+  return null;
+};
+
+const sortFilmDateUp = (filmA, filmB) => {
+  const { film_info: { release: { date: dateA } } } = filmA;
+  const { film_info: { release: { date: dateB } } } = filmB;
+  const weight = getWeightForNullDate(dateA, dateB);
+
+  if (weight !== null) {
+    return weight;
+  }
+
+  return dayjs(dateA).diff(dayjs(dateB));
+};
+
+const sortFilmDateDown = (filmA, filmB) => {
+  const { film_info: { release: { date: dateA } } } = filmA;
+  const { film_info: { release: { date: dateB } } } = filmB;
+  const weight = getWeightForNullDate(dateA, dateB);
+
+  if (weight !== null) {
+    return weight;
+  }
+
+  return dayjs(dateB).diff(dayjs(dateA));
+};
+
+const sortFilmRatingUp = (filmA, filmB) => {
+  const { film_info: { rating: ratingA } } = filmA;
+  const { film_info: { rating: ratingB } } = filmB;
+  return ratingA - ratingB;
+};
+
+const sortFilmRatingDown = (filmA, filmB) => {
+  const { film_info: { rating: ratingA } } = filmA;
+  const { film_info: { rating: ratingB } } = filmB;
+  return ratingB - ratingA;
+};
+
+export {
+  humanizeDate,
+  sortFilmDateUp,
+  sortFilmDateDown,
+  sortFilmRatingUp,
+  sortFilmRatingDown
+};

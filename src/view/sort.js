99293091fc93f03e10bcd17d42f26ab107f50515
Maksim Kalinin <kalinin.m.a@bk.ru>
2021-05-01 16:05:58 +0300

    Добавляет метод подписки на изменение сортировки

--- src/view/sort.js
+++ src/view/sort.js
@@ -1,12 +1,37 @@
 import AbstractView from './abstract';
 
+const SortType = {
+  DEFAULT: 'default',
+  DATE_UP: 'date-up',
+  DATE_DOWN: 'date-down',
+  RATING_UP: 'rating-up',
+  RATING_DOWN: 'rating-down',
+};
+
 class Sort extends AbstractView {
+  constructor() {
+    super();
+
+    this._sortTypeChangeHandler = this._sortTypeChangeHandler.bind(this);
+  }
   getTemplate() {
     return `<ul class="sort">
-      <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-      <li><a href="#" class="sort__button">Sort by date</a></li>
-      <li><a href="#" class="sort__button">Sort by rating</a></li>
+      <li><a href="#" class="sort__button sort__button--active" data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
+      <li><a href="#" class="sort__button" data-sort-type="${SortType.DATE_DOWN}">Sort by date</a></li>
+      <li><a href="#" class="sort__button" data-sort-type="${SortType.RATING_DOWN}">Sort by rating</a></li>
     </ul >`;
+  }
+
+  _sortTypeChangeHandler(evt) {
+    if (evt.target.tagName !== 'A') return;
+
+    evt.preventDefault();
+    this._callback.sortTypeChange(evt.target.dataset.sortType);
+  }
+
+  setSortTypeChangeHandler(callback) {
+    this._callback.sortTypeChange = callback;
+    this.getElement().addEventListener('click', this._sortTypeChangeHandler);
   }
 }
 

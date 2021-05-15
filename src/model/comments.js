0295d9388e4923b604a8924f76077cab291c5927
Maksim Kalinin <kalinin.m.a@bk.ru>
2021-05-15 15:37:45 +0300

    Добавляет методы добавления/удаления/обновления в модель

--- src/model/comments.js
+++ src/model/comments.js
@@ -13,6 +13,30 @@ class Comments extends Observer {
   getComments() {
     return this._comments;
   }
+
+  addComment(updateType, update) {
+    this._comments = [
+      update,
+      ...this._comments,
+    ];
+
+    this._notify(updateType, update);
+  }
+
+  deleteComment(updateType, update) {
+    const index = this._comments.findIndex((comment) => comment.id === update.id);
+
+    if (index === -1) {
+      throw new Error('Can\'t delete unexisting comment');
+    }
+
+    this._comments = [
+      ...this._comments.slice(0, index),
+      ...this._comments.slice(index + 1),
+    ];
+
+    this._notify(updateType);
+  }
 }
 
 export { Comments as default };

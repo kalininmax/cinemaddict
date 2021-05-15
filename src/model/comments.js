fd124b3c4ba240cf5dcf2805bdbb9025079f18ff
Maksim Kalinin <kalinin.m.a@bk.ru>
2021-05-15 10:00:05 +0300

    Добавляет модель для фильмов и комментариев

---
+++ src/model/comments.js
@@ -0,0 +1,18 @@
+import Observer from '../utils/observer';
+
+class Comments extends Observer {
+  constructor() {
+    super();
+    this._comments = [];
+  }
+
+  setComments(comments) {
+    this._comments = comments.slice();
+  }
+
+  getComments() {
+    return this._comments;
+  }
+}
+
+export { Comments as default };
---
+++ src/model/movies.js
@@ -0,0 +1,18 @@
+import Observer from '../utils/observer';
+
+class Movies extends Observer {
+  constructor() {
+    super();
+    this._movies = [];
+  }
+
+  setMovies(movies) {
+    this._movies = movies.slice();
+  }
+
+  getMovies() {
+    return this._movies;
+  }
+}
+
+export { Movies as default };

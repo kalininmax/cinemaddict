afb0674dd391135c13ffc001f66cc9ab1b9020ef
Maksim Kalinin <kalinin.m.a@bk.ru>
2021-05-27 01:05:14 +0300

    Инициализирует презентер фильтра

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import { generateFilm } from './mock/film';
-import { generateFilter } from './mock/filter';
 import { allComments } from './mock/comment';
 import { render, RenderPosition } from './utils/render';
 import HeaderProfileView from './view/header-profile';
 import FooterStatisticView from './view/footer-statistics';
 import MoviePresenter from './presenter/movie-list';
+import FilterPresenter from './presenter/filter';
 import MoviesModel from './model/movies';
 import CommentsModel from './model/comments';
 import FilterModel from './model/filter';
@@ -12,7 +12,6 @@ import FilterModel from './model/filter';
 const FILMS_COUNT = 12;
 
 const films = new Array(FILMS_COUNT).fill().map(generateFilm);
-const filters = generateFilter(films);
 
 const moviesModel = new MoviesModel();
 moviesModel.setMovies(films);
@@ -26,9 +25,11 @@ const filterModel = new FilterModel();
 const headerElement = document.querySelector('.header');
 const mainElement = document.querySelector('.main');
 const footerStats = document.querySelector('.footer__statistics');
-const moviePresenter = new MoviePresenter(mainElement, moviesModel, filters, commentsModel);
+const moviePresenter = new MoviePresenter(mainElement, moviesModel, commentsModel, filterModel);
+const filterPresenter = new FilterPresenter(mainElement, filterModel, moviesModel);
 
 render(headerElement, new HeaderProfileView(), RenderPosition.BEFOREEND);
 render(footerStats, new FooterStatisticView(films.length), RenderPosition.BEFOREEND);
 
+filterPresenter.init();
 moviePresenter.init();

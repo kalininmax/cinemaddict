f687ee57bbce4ce06394045d2db4559b417f2a48
Maksim Kalinin <kalinin.m.a@bk.ru>
2021-05-26 22:40:17 +0300

    Добавляет передачу модели комментариев в презентер списка

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { generateFilm } from './mock/film';
-import { allComments } from './mock/comment';
 import { generateFilter } from './mock/filter';
+import { allComments } from './mock/comment';
 import { render, RenderPosition } from './utils/render';
 import HeaderProfileView from './view/header-profile';
 import FooterStatisticView from './view/footer-statistics';
@@ -11,20 +11,21 @@ import CommentsModel from './model/comments';
 const FILMS_COUNT = 12;
 
 const films = new Array(FILMS_COUNT).fill().map(generateFilm);
-const comments = allComments.slice();
 const filters = generateFilter(films);
 
 const moviesModel = new MoviesModel();
 moviesModel.setMovies(films);
+
 const commentsModel = new CommentsModel();
-commentsModel.setComments(comments);
+commentsModel.setComments(allComments);
+
 
 const headerElement = document.querySelector('.header');
 const mainElement = document.querySelector('.main');
 const footerStats = document.querySelector('.footer__statistics');
+const moviePresenter = new MoviePresenter(mainElement, moviesModel, filters, commentsModel);
 
 render(headerElement, new HeaderProfileView(), RenderPosition.BEFOREEND);
 render(footerStats, new FooterStatisticView(films.length), RenderPosition.BEFOREEND);
 
-const moviePresenter = new MoviePresenter(mainElement, moviesModel, commentsModel, filters);
 moviePresenter.init();

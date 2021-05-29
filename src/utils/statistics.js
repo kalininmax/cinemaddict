086c12432e03c10cd417c27f6ad06ce7ef3c79ac
Maksim Kalinin <kalinin.m.a@bk.ru>
2021-05-29 19:43:56 +0300

    Добавляет перечисление для графика

--- src/utils/statistics.js
+++ src/utils/statistics.js
@@ -1,36 +1,74 @@
 import dayjs from 'dayjs';
-import { StatsDate } from '../const';
 
-const getStatsTime = (timeInterval) => {
-  let dateFrom = dayjs(), dateTo;
-
-  switch (timeInterval) {
-    case StatsDate.ALL_TIME.type:
-      dateFrom = null;
-      dateTo = null;
-      break;
-
-    case StatsDate.TODAY.type:
-      dateTo = dayjs().subtract(1, 'day');
-      break;
-
-    case StatsDate.WEEK.type:
-      dateTo = dayjs().subtract(7, 'day');
-      break;
-
-    case StatsDate.MONTH.type:
-      dateTo = dayjs().subtract(30, 'day');
-      break;
-
-    case StatsDate.YEAR.type:
-      dateTo = dayjs().subtract(365, 'day');
-      break;
-
-    default:
-      break;
-  }
-
-  return [dateFrom, dateTo];
+const CHART_BAR = {
+  TYPE: 'horizontalBar',
+  HEIGHT: 50,
+  THICKNESS: 24,
+  PADDING: 100,
+  LABEL_OFFSET: 40,
+  LABEL_ALIGN: 'start',
+  FONT_SIZE: 20,
+  FONT_COLOR: '#fff',
+  BG_COLOR: '#ffe800',
 };
 
-export { getStatsTime };
+const filmsToFilterMap = {
+  'all-time': (films) => films.filter(({ user_details: { watched } }) => watched),
+  today: (films) => films
+    .filter(({ user_details: { watched } }) => watched)
+    .filter(({ user_details: { watchingDate } }) => watchingDate > dayjs().subtract(1, 'day')),
+  week: (films) => films
+    .filter(({ user_details: { watched } }) => watched)
+    .filter(({ user_details: { watchingDate } }) => watchingDate > dayjs().subtract(7, 'day')),
+  month: (films) => films
+    .filter(({ user_details: { watched } }) => watched)
+    .filter(({ user_details: { watchingDate } }) => watchingDate > dayjs().subtract(30, 'day')),
+  year: (films) => films
+    .filter(({ user_details: { watched } }) => watched)
+    .filter(({ user_details: { watchingDate } }) => watchingDate > dayjs().subtract(365, 'day')),
+};
+
+const getTotalDuration = (films) => {
+  if (films.length === 0) {
+    return '';
+  }
+
+  return films
+    .map(({ film_info: { runtime } }) => runtime)
+    .reduce((a, b) => a + b, 0);
+};
+
+const getGenresStatistics = (films) => {
+  const genresStatistics = {};
+
+  films
+    .reduce((acc, film) => acc.concat(film.film_info.genres), [])
+    .forEach((genre) => {
+      if (genresStatistics[genre]) {
+        genresStatistics[genre]++;
+        return;
+      }
+      genresStatistics[genre] = 1;
+    });
+
+  return genresStatistics;
+};
+
+const getTopGenre = (films) => {
+  if (films.length === 0) {
+    return '';
+  }
+
+  const genresStatistics = getGenresStatistics(films);
+  const topGenreStatistics = Object.entries(genresStatistics).sort((a, b) => b[1] - a[1])[0];
+  const topGenreName = topGenreStatistics[0];
+  return topGenreName;
+};
+
+export {
+  filmsToFilterMap,
+  getTotalDuration,
+  getGenresStatistics,
+  getTopGenre,
+  CHART_BAR
+};

1affd8a8918ec8a31b762352ee3f9e8d1bd681d3
Maksim Kalinin <kalinin.m.a@bk.ru>
2021-05-29 17:44:49 +0300

    Добавляет представление статистики

--- src/view/statistics.js
+++ src/view/statistics.js
@@ -1,6 +1,71 @@
+import dayjs from 'dayjs';
+import Chart from 'chart.js';
+import { getHourFromMin } from '../utils/film';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 import SmartView from './smart';
+import { getStatsTime } from '../utils/statistics';
+import { StatsDate } from '../const';
 
-const createStatisticsTemplate = () => {
+const filmsToFilterMap = {
+  'all-time': (films) => films.filter(({ user_details: { watched } }) => watched),
+  today: (films) => films
+    .filter(({ user_details: { watched } }) => watched)
+    .filter(({ user_details: { watchingDate } }) => watchingDate > dayjs().subtract(1, 'day')),
+  week: (films) => films
+    .filter(({ user_details: { watched } }) => watched)
+    .filter(({ user_details: { watchingDate } }) => watchingDate > dayjs().subtract(7, 'day')),
+  month: (films) => films
+    .filter(({ user_details: { watched } }) => watched)
+    .filter(({ user_details: { watchingDate } }) => watchingDate > dayjs().subtract(30, 'day')),
+  year: (films) => films
+    .filter(({ user_details: { watched } }) => watched)
+    .filter(({ user_details: { watchingDate } }) => watchingDate > dayjs().subtract(365, 'day')),
+};
+
+const getTotalDuration = (films) => {
+  return films
+    .map(({ film_info: { runtime } }) => runtime)
+    .reduce((a, b) => a + b, 0);
+};
+
+const createStatsFilterTemplate = (currentFilter) => {
+  return `<form action="https://echo.htmlacademy.ru/" method="get" class="statistic__filters">
+  <p class="statistic__filters-description">Show stats:</p>
+
+  <input type="radio"
+  class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-all-time"
+  value="${StatsDate.ALL_TIME.type}"
+  ${currentFilter === StatsDate.ALL_TIME.type ? 'checked' : ''}>
+  <label for="statistic-all-time" class="statistic__filters-label">${StatsDate.ALL_TIME.name}</label>
+
+  <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-today"
+  value="${StatsDate.TODAY.type}"
+  ${currentFilter === StatsDate.TODAY.type ? 'checked' : ''}>
+  <label for="statistic-today" class="statistic__filters-label">${StatsDate.TODAY.name}</label>
+
+  <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-week"
+  value="${StatsDate.WEEK.type}"
+  ${currentFilter === StatsDate.WEEK.type ? 'checked' : ''}>
+  <label for="statistic-week" class="statistic__filters-label">${StatsDate.WEEK.name}</label>
+
+  <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-month"
+  value="${StatsDate.MONTH.type}"
+  ${currentFilter === StatsDate.MONTH.type ? 'checked' : ''}>
+  <label for="statistic-month" class="statistic__filters-label">${StatsDate.MONTH.name}</label>
+
+  <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-year"
+  value="${StatsDate.YEAR.type}"
+  ${currentFilter === StatsDate.YEAR.type ? 'checked' : ''}>
+  <label for="statistic-year" class="statistic__filters-label">${StatsDate.YEAR.name}</label>
+</form>`;
+};
+
+const createStatisticsTemplate = (films, currentFilter) => {
+  const filteredFilms = filmsToFilterMap[currentFilter](films);
+  console.log(filteredFilms);
+
+  const totalDuration = getTotalDuration(filteredFilms);
+
   return `<section class="statistic">
     <p class="statistic__rank">
       Your rank
@@ -8,33 +73,16 @@ const createStatisticsTemplate = () => {
       <span class="statistic__rank-label">Movie buff</span>
     </p>
 
-    <form action="https://echo.htmlacademy.ru/" method="get" class="statistic__filters">
-      <p class="statistic__filters-description">Show stats:</p>
-
-      <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-all-time" value="all-time" checked>
-      <label for="statistic-all-time" class="statistic__filters-label">All time</label>
-
-      <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-today" value="today">
-      <label for="statistic-today" class="statistic__filters-label">Today</label>
-
-      <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-week" value="week">
-      <label for="statistic-week" class="statistic__filters-label">Week</label>
-
-      <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-month" value="month">
-      <label for="statistic-month" class="statistic__filters-label">Month</label>
-
-      <input type="radio" class="statistic__filters-input visually-hidden" name="statistic-filter" id="statistic-year" value="year">
-      <label for="statistic-year" class="statistic__filters-label">Year</label>
-    </form>
+    ${createStatsFilterTemplate(currentFilter)}
 
     <ul class="statistic__text-list">
       <li class="statistic__text-item">
         <h4 class="statistic__item-title">You watched</h4>
-        <p class="statistic__item-text"><span class="statistic__item-description">movies</span></p>
+        <p class="statistic__item-text">${filteredFilms.length} <span class="statistic__item-description">movies</span></p>
       </li>
       <li class="statistic__text-item">
         <h4 class="statistic__item-title">Total duration</h4>
-        <p class="statistic__item-text">130 <span class="statistic__item-description">h</span> 22 <span class="statistic__item-description">m</span></p>
+        <p class="statistic__item-text">${totalDuration} <span class="statistic__item-description">h</span> 22 <span class="statistic__item-description">m</span></p>
       </li>
       <li class="statistic__text-item">
         <h4 class="statistic__item-title">Top genre</h4>
@@ -49,13 +97,40 @@ const createStatisticsTemplate = () => {
 };
 
 class Statistics extends SmartView {
-  constructor() {
+  constructor(films) {
     super();
+
+    this._data = films;
+
+    this._currentFilter = StatsDate.ALL_TIME.type;
+
+    this._statsFilterChangeHandler = this._statsFilterChangeHandler.bind(this);
+    this._setStatsFilterChangeHandler();
+
+  }
+
+  restoreHandlers() {
+    this._setStatsFilterChangeHandler();
   }
 
   getTemplate() {
-    return createStatisticsTemplate();
+    console.log(11, this._data.slice(), this._currentFilter);
+
+    return createStatisticsTemplate(this._data, this._currentFilter);
   }
+
+  _statsFilterChangeHandler(evt) {
+    evt.preventDefault();
+    if (evt.target.classList.contains('statistic__filters-input')) {
+      this._currentFilter = evt.target.value;
+      this.updateElement();
+    }
+  }
+
+  _setStatsFilterChangeHandler() {
+    this.getElement().addEventListener('change', this._statsFilterChangeHandler);
+  }
+
 }
 
 export { Statistics as default };

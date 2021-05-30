967b3214f40e782721ace4c21168aa1be7f3ff72
Maksim Kalinin <kalinin.m.a@bk.ru>
2021-05-30 10:56:25 +0300

    Добавляет "адаптер" в модель фильмов

--- src/model/movies.js
+++ src/model/movies.js
@@ -29,6 +29,80 @@ class Movies extends Observer {
 
     this._notify(updateType, update);
   }
+
+  static adaptToClient(film) {
+    const adaptedFilm = Object.assign(
+      {},
+      film,
+      {
+        filmInfo: {
+          title: film.film_info.title,
+          alternativeTitle: film.film_info.alternative_title,
+          rating: film.film_info.total_rating,
+          poster: film.film_info.poster,
+          ageRating: film.film_info.age_rating,
+          director: film.film_info.director,
+          writers: film.film_info.writers,
+          actors: film.film_info.actors,
+          release: {
+            date: film.film_info.release.date,
+            country: film.film_info.release.release_country,
+          },
+          runtime: film.film_info.runtime,
+          genres: film.film_info.genre,
+          description: film.film_info.description,
+        },
+        userDetails: {
+          watchlist: film.user_details.watchlist,
+          watched: film.user_details.already_watched,
+          watchingDate: film.user_details.watching_date,
+          favorite: film.user_details.favorite,
+        },
+      },
+    );
+
+    delete adaptedFilm.film_info;
+    delete adaptedFilm.user_details;
+
+    return adaptedFilm;
+  }
+
+  static adaptToServer(film) {
+    const adaptedFilm = Object.assign(
+      {},
+      film,
+      {
+        'film_info': {
+          'title': film.filmInfo.title,
+          'alternative_title': film.filmInfo.alternativeTitle,
+          'poster': film.filmInfo.poster,
+          'description': film.filmInfo.description,
+          'total_rating': film.filmInfo.rating,
+          'release': {
+            'date': film.filmInfo.release.date,
+            'release_country': film.filmInfo.release.country,
+          },
+          'runtime': film.filmInfo.runtime,
+          'genre': film.filmInfo.genres,
+          'director': film.filmInfo.director,
+          'writers': film.filmInfo.writers,
+          'actors': film.filmInfo.actors,
+          'age_rating': film.filmInfo.ageRating,
+        },
+        'user_details': {
+          'watchlist': film.userDetails.watchlist,
+          'already_watched': film.userDetails.watched,
+          'watching_date': film.userDetails.watchingDate,
+          'favorite': film.userDetails.favorite,
+        },
+      },
+    );
+
+    delete adaptedFilm.filmInfo;
+    delete adaptedFilm.userDetails;
+
+    return adaptedFilm;
+  }
 }
 
 export { Movies as default };

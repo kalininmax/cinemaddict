142416839d68e07091456efb1ae845c1fc7f7852
Maksim Kalinin <kalinin.m.a@bk.ru>
2021-05-30 11:24:54 +0300

    Добавляет представление для профиля пользователя

---
+++ src/view/user-profile.js
@@ -0,0 +1,17 @@
+import AbstractView from './abstract';
+
+class UserProfile extends AbstractView {
+  constructor(rank) {
+    super();
+    this._rank = rank;
+  }
+
+  getTemplate() {
+    return `<section class="header__profile profile">
+    <p class="profile__rating">${this._rank}</p>
+    <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
+  </section>`;
+  }
+}
+
+export { UserProfile as default };
